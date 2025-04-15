2fd7ebf7767b19052696ad7dd5f923a455565ada
DrushtiGP <drushtip.07@gmai.com>
2025-04-16 00:38:00 +0530

    backend init

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+DB_HOST=turntable.proxy.rlwy.net
+DB_PORT=23751
+DB_USER=root
+DB_PASSWORD=jSEVGPPByWdDakKGjQqMVzBKoJHsAlXq
+DB_NAME=railway
+// Load from env variables
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT)
+  });
+
+db.connect(err => {
+  if (err) console.error('DB Error: ', err);
+  else console.log('Connected to MySQL');
+});
+
+app.get('/notes', (req, res) => {
+  db.query('SELECT * FROM notes', (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+app.post('/notes', (req, res) => {
+  const { title, content } = req.body;
+  db.query('INSERT INTO notes (title, content) VALUES (?, ?)', [title, content], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.send({ id: result.insertId, title, content });
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
